bac74aa7b7424644195159a325cec54b2a5d3da9
Ruben Kerkhofs <44324476+rubenkerkhofs@users.noreply.github.com>
2021-11-22 13:17:15 +0100

    Day 2

---
+++ 4. Streamlit/text-types.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Title")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.caption("This is a caption")
+st.code("text = 'This is a code block'")
+st.text("This     is some    preformated \ \ text")
+st.latex("X \sim \mathcal{N}(\mu,\,\sigma^{2})")
